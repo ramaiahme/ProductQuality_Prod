729b4945ffbe504bfd15bba80bbb800b77391fa8
Ramaiah Chidambaram <82108425+ramaiahme@users.noreply.github.com>
2025-02-02 22:49:48 +0530

    Update app.py
    changing port

--- app.py
+++ app.py
@@ -82,4 +82,4 @@ async def predict_quality(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info")
